41f1b5ad56d2d1c5b0a07d101dac1cbc74f98017
Shivam Kumar <engineeraccidental@gmail.com>
2017-07-11 18:25:04 +0530

    Styled the footer section
    I think not using em to set font size is proving to be a big mistake

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-  if($(window).width() < 500) $(".link-label").hide();
+  if($(window).width() < 500) $(".link-label, .link-label-footer").hide();
 
-  if($(window).width() < 800)  $(".tab").css({"width": "20%", "font-size": "0.6em"});
+  if($(window).width() < 800)  $(".tab").css({"width": "20%", "font-size": "0.65em"});
 
   $(window).resize(function(){
 
-    if($(window).width() < 500) $(".link-label").hide();
-    else  $(".link-label").show();
+    if($(window).width() < 500) $(".link-label, .link-label-footer").hide();
+    else  $(".link-label, .link-label-footer").show();
 
-    if($(window).width() < 800)  $(".tab").css({"width": "20%", "font-size": "0.6em"});
+    if($(window).width() < 800)  $(".tab").css({"width": "20%", "font-size": "0.65em"});
     else $(".tab").css({"width": "12%", "font-size": "0.8em"});
 
   });
